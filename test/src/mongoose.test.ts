7bd4abed697386f780479ebefa4aca2ed98a625d
demingongo <sdemingongo@gmail.com>
2024-07-13 12:11:27 +0200

    refactor everything wip

---
+++ test/src/mongoose.test.ts
@@ -0,0 +1,62 @@
+import { createConnection/*, HydratedDocument*/ } from 'mongoose'
+import { expect } from 'chai';
+
+import { MovieSettings, MovieJson, MovieModel } from './movieSchema';
+
+describe('just mongoose', function () {
+    const { logger, params } = this.ctx.kaukau;
+
+    it('should init and connect', async () => {
+
+        let databaseUri = `${params('mongodb.protocol')}://`;
+        if (params('mongodb.username') && params('mongodb.password')) {
+            databaseUri += `${params('mongodb.username')}:${params('mongodb.password')}@`;
+        }
+        databaseUri += `${params('mongodb.hostname')}`;
+        if (params('mongodb.port') && params('mongodb.port') !== '0') {
+            databaseUri += `:${params('mongodb.port')}`;
+        }
+        databaseUri += `/${params('mongodb.database')}`;
+        if (params('mongodb.options')) {
+            databaseUri += `?${params('mongodb.options')}`;
+        }
+
+        const connection = await createConnection(databaseUri, { maxPoolSize: 24 }).asPromise()
+
+        try {
+            logger.log('retrieved connection for database', connection.name);
+            expect(connection.name).to.be.a('string');
+
+
+            const Movies = connection.model<MovieJson, MovieModel>(MovieSettings.name, MovieSettings.schema, MovieSettings.collection);
+
+            //const Movies = connection.model<MovieJson>(MovieSettings.name)
+
+            logger.log('static method', Movies.myStaticMethod())
+
+            const title = `Last peasant ${Math.ceil(Math.random() * 1000) + 1}`
+
+            const newMovie/*: HydratedDocument<MovieJson>*/ = new Movies({ rate: 5, title })
+            await newMovie.save()
+            logger.log('fullname()', newMovie.fullName())
+
+            const obj = newMovie.toObject({ virtuals: true })
+            logger.info('obj', obj)
+
+            logger.log('Query Helper ".byTitle(...)"', await Movies.find({}).byTitle(title))
+
+            logger.info('deleted', await newMovie.deleteOne())
+
+            await connection.close()
+            logger.info('closed connections');
+
+            logger.info('Done');
+        } catch(e) {
+            await connection.close()
+            logger.info('closed connections');
+            throw e;
+        }
+
+        
+    });
+});
--- test/src/movieSchema.ts
+++ test/src/movieSchema.ts
@@ -1,6 +1,9 @@
-import { Document, Schema } from 'mongoose';
-import { ModelSettings } from '../../src/index';
+import { Document, Schema, Model, QueryWithHelpers, HydratedDocument } from 'mongoose';
+//import { ModelSettings } from '../../src/index';
 
+/**
+ * the object as it is stored in MongoDB
+ */
 export interface MovieJson {
   title: string;
   rate?: number;
@@ -9,20 +12,98 @@ export interface MovieJson {
 export interface Movie extends Document, MovieJson {
 }
 
-export const MovieSettings: ModelSettings = {
+// Put all user instance methods in this interface:
+export interface MovieJsonMethods {
+  fullName(): string;
+}
+
+export interface MovieModelStatics {
+  myStaticMethod(): number;
+}
+
+// Create a new Model type that knows about IUserMethods...
+//export type MovieModel = Model<MovieJson, NonNullable<unknown>, MovieJsonMethods> & MovieModelStatics;
+
+export interface MovieQueryHelpers {
+  byTitle(title: string): QueryWithHelpers<
+    HydratedDocument<MovieJson>[],
+    HydratedDocument<MovieJson>,
+    MovieQueryHelpers
+  >
+}
+
+export interface MovieVirtuals {
+  displayName: string
+}
+
+export interface MovieModel extends Model<MovieJson, MovieQueryHelpers, MovieJsonMethods>, MovieModelStatics {
+}
+
+const movieSchema = new Schema<MovieJson, MovieModel, MovieJsonMethods, MovieQueryHelpers, MovieVirtuals>({
+  title: {
+    type: String,
+    trim: true,
+    required: [true, '\'title\' is required.'],
+    unique: true,
+    index: { unique: true },
+  },
+  rate: {
+    type: Number,
+    default: 0
+  },
+},
+{
+  /*
+  virtuals: {
+    displayName: {
+      get: function() {
+        return `${this.title} (${this.rate} stars)`
+      }
+    }
+  },
+  */
+  /*
+  methods: {
+    fullName() {
+      return `${this.title} (${this.rate} stars)`
+    },
+  },
+  */
+  /*
+  query: {
+    byTitle(title: string) {
+      return this.find({ title: title });
+    }
+  },
+  */
+  statics: {
+    myStaticMethod() {
+      return 42;
+    }
+  }
+})
+
+movieSchema.virtual('displayName').get(function () {
+  return `${this.title} (${this.rate} stars)`
+});
+
+movieSchema.methods.fullName = function() {
+  return `${this.title} (${this.rate} stars)`
+}
+
+movieSchema.query.byTitle = function(
+  this: QueryWithHelpers<unknown, HydratedDocument<MovieJson>, MovieQueryHelpers>,
+  title: string
+) {
+  return this.find({ title: title });
+}
+
+
+export const MovieSettings/*: ModelSettings*/ = {
     name: 'movies',
-    schema: new Schema({
-      title: {
-        type: String,
-        trim: true,
-        required: [true, '\'title\' is required.'],
-        unique: true,
-        index: { unique: true },
-      },
-      rate: {
-        type: Number,
-        default: 0
-      },
-    }),
+    schema: movieSchema,
     collection: 'movies'
 };
+
+
+//------------------------
